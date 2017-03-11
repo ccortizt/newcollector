45c713d6845768aedd9b1abbf041a547f5fa5dbb
ccortizt <ccamilo0203@gmail.com>
2017-03-10 21:43:18 -0500

    Added IA Item Shot ability

--- Collector2/Assets/Scripts/Standalone/IAPlayerController.cs
+++ Collector2/Assets/Scripts/Standalone/IAPlayerController.cs
@@ -14,8 +14,12 @@ public class IAPlayerController : MonoBehaviour
     public float moveSpeedPercentage = 100;
     private float defaultMoveSpeedPercentage = 100;
     private float effectTime = 4f;
-    
 
+    GameObject player;
+
+    public GameObject iceProp;
+    public GameObject slowProp;
+    public GameObject speedProp;
 
     void OnAwake()
     {
@@ -23,13 +27,124 @@ public class IAPlayerController : MonoBehaviour
     }
     void Start()
     {
+        player = GameObject.Find("Player");
         gameObject.transform.FindChild("Rabbit_Yellow").gameObject.SetActive(true);
         gameObject.transform.FindChild("Rabbit_Red").gameObject.SetActive(false);
         gameObject.transform.FindChild("Canvas/Text").GetComponent<Text>().color = Color.black;
+
+
     }
 
     void Update()
     {
+        
+        if(!GetComponent<PlayerController>().GetPowerAcquired().Equals("none")){
+
+            string powerAcquired = GetComponent<PlayerController>().GetPowerAcquired();
+
+            var prop = iceProp;
+
+            if ((int)player.transform.position.x == (int)transform.position.x)
+            {
+                if (player.transform.position.y < transform.position.y)
+                {
+                    if (powerAcquired.Equals("ice"))
+                    {
+                        prop = Instantiate(iceProp, transform.position - transform.up, Quaternion.identity);
+                    }
+
+                    if (powerAcquired.Equals("slow"))
+                    {
+                        prop = Instantiate(slowProp, transform.position - transform.up, Quaternion.identity);
+                    }
+
+                    if (powerAcquired.Equals("speed"))
+                    {
+                        prop = Instantiate(speedProp, transform.position - transform.up, Quaternion.identity);
+                    }
+                    
+                    GetComponent<PlayerController>().SetPowerAcquired("none");
+
+                    prop.GetComponent<AppleController>().SetMovementDisabled();
+                    prop.GetComponent<Rigidbody>().velocity = -prop.transform.right * 20;
+                }
+                else
+                {
+
+                    if (powerAcquired.Equals("ice"))
+                    {
+                        prop = Instantiate(iceProp, transform.position + transform.up, Quaternion.identity);
+                    }
+
+                    if (powerAcquired.Equals("slow"))
+                    {
+                        prop = Instantiate(slowProp, transform.position + transform.up, Quaternion.identity);
+                    }
+
+                    if (powerAcquired.Equals("speed"))
+                    {
+                        prop = Instantiate(speedProp, transform.position + transform.up, Quaternion.identity);
+                    }
+
+                     GetComponent<PlayerController>().SetPowerAcquired("none");
+
+                    prop.GetComponent<AppleController>().SetMovementDisabled();
+                    prop.GetComponent<Rigidbody>().velocity = prop.transform.right * 20;
+                }
+            }
+
+            if ((int)player.transform.position.y == (int)transform.position.y)
+            {
+                if (player.transform.position.y < transform.position.y)
+                {
+
+                    if (powerAcquired.Equals("ice"))
+                    {
+                        prop = Instantiate(iceProp, transform.position - transform.right, Quaternion.identity);
+
+                    }
+
+                    if (powerAcquired.Equals("slow"))
+                    {
+                        prop = Instantiate(slowProp, transform.position - transform.right, Quaternion.identity);
+                    }
+
+                    if (powerAcquired.Equals("speed"))
+                    {
+                        prop = Instantiate(speedProp, transform.position - transform.right, Quaternion.identity);
+                    }
+
+                    GetComponent<PlayerController>().SetPowerAcquired("none");
+
+                    prop.GetComponent<AppleController>().SetMovementDisabled();
+                    prop.GetComponent<Rigidbody>().velocity = prop.transform.forward * 20;
+                }
+                else
+                {
+
+                    if (powerAcquired.Equals("ice"))
+                    {
+                        prop = Instantiate(iceProp, transform.position + transform.right, Quaternion.identity);
+                    }
+
+                    if (powerAcquired.Equals("slow"))
+                    {
+                        prop = Instantiate(slowProp, transform.position + transform.right, Quaternion.identity);
+                    }
+
+                    if (powerAcquired.Equals("speed"))
+                    {
+                        prop = Instantiate(speedProp, transform.position + transform.right, Quaternion.identity);
+                    }
+
+                    GetComponent<PlayerController>().SetPowerAcquired("none");
+
+                    prop.GetComponent<AppleController>().SetMovementDisabled();
+                    prop.GetComponent<Rigidbody>().velocity = -prop.transform.forward * 20;
+                }
+            }
+        }      
+
 
         transform.Rotate(new Vector3(90, 0, 0));
         transform.position += new Vector3(0, 0.4f, 0);
